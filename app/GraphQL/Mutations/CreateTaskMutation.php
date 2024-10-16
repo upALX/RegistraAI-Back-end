bf43dbf2cbd2f882ce3ce5f7717a0ac2b25bbab0
José Alexandre <alexandreaddres@gmail.com>
2024-10-15 23:06:37 -0300

    fixing mutations schema

--- app/GraphQL/Mutations/CreateTaskMutation.php
+++ app/GraphQL/Mutations/CreateTaskMutation.php
@@ -1,35 +1,32 @@
 <?php
 
+namespace App\GraphQL\Mutations;
+
 use App\Jobs\SendTaskMailJob; 
 use App\Models\Task;
 use Illuminate\Support\Facades\Auth;
+use GraphQL\Error\Error;
 
 class CreateTaskMutation
 {
     public function __invoke($_, array $args, $context)
     {
-        // Verifica se o usuário está autenticado
         $user = Auth::user();
-        // if (!$user) {
-        //     return [
-        //         'success' => false,
-        //         'message' => 'User must be authenticated to create a task.',
-        //     ];
-        // }
+        if (!$user) {
+            throw new Error('User must be auth.');
+        }
 
-        // Cria a tarefa
         $task = Task::create([
             'title' => $args['title'],
             'description' => $args['description'],
             'user_id' => $user->id,
         ]);
 
-        // Dispara o job para enviar o email
         (new SendTaskMailJob($task))->dispatch();
 
         return [
             'success' => true,
-            'message' => 'Task created successfully.',
+            'message' => 'Tarefa criada com sucesso.',
             'task' => $task,
         ];
     }
